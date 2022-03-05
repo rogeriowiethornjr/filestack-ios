2ef0b0b2b705c7cc4aadeca08751eaefdb325ea7
rogeriowiethornjr <36937774+rogeriowiethornjr@users.noreply.github.com>
2022-03-04 23:51:43 -0300

    Update Stylizer.swift

--- Sources/Filestack/UI/Public/Models/Stylizer.swift
+++ Sources/Filestack/UI/Public/Models/Stylizer.swift
@@ -65,7 +65,7 @@ import UIKit
             }
         }()
 
-        fileprivate(set) var title = "Filestack"
+        fileprivate(set) var title = "Selecionar arquivos"
     }
 
     struct NavigationBarViewModel {
